a37e9b2150bcd8cffc486c8c616bf2482ae80522
Macatucano <duduuhuah@gmail.com>
2024-07-10 16:52:34 -0300

    Adicionando palavras

---
+++ palavras.js
@@ -0,0 +1,66 @@
+const palavras = [
+    {
+        nome: "Banana",
+        categoria: "fruta"
+    },
+    {
+        nome: "Alface",
+        categoria: "verdura"
+    },
+    {
+        nome: "Jumento",
+        categoria: "animal"
+    },
+    {
+        nome: "Inhame",
+        categoria: "comida"
+    },
+    {
+        nome: "Gargantilha",
+        categoria: "acessório" 
+    },
+    {
+        nome: "Barba Ruiva",
+        categoria: "folclore"
+    },
+    {
+        nome: "Tacape",
+        categoria: "arma"
+    },
+    {
+        nome: "Profissão",
+        categoria: "economista"
+    },
+    {
+        nome: "Empecilho",
+        categoria: "bicho de sete cabeças"
+    },
+    {
+        nome: "Psicanálise",
+        categoria: "estudo Teórico"
+    },
+    {
+        nome: "Cãibra",
+        categoria: "dor"
+    },
+    {
+        nome: "Lazulita",
+        categoria: "Mineral"
+    },
+    {
+        nome: "Jazz",
+        categoria: "estilo musical",
+    },
+    {
+        nome: "Echarpe",
+        categoria: "objeto"
+    },
+    {
+        nome: "Vestíbulo",
+        categoria: "pátio de acesso à entrada de uma construção"
+    },
+    {
+        nome: "Narguilé",
+        categoria: "tipo de cachimbo"
+    }
+]
