790892eb99b5abb18494f79d03e4da42e683a778
elder51 <elder2051@gmail.com>
2024-07-24 12:13:09 -0300

    1.24.1

---
+++ palavras-comidas.js
@@ -0,0 +1,705 @@
+const palavras1 = [
+    {
+        palavra : "Amendoim",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Arroz",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Amora",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Açaí",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Abacaxi",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Bolo",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Banana",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Brigadeiro",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Bolacha",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Beringela",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Camarão",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Coxinha",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Charque",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Cuscuz",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Cachorro quente",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Chocolate",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Damasco",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Dobradinha",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Doce de leite",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Donuts",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Empada",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Enroladinho",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Empadão",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Espaguete",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Ervilha",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Framboesa",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Feijão",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Frango",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Figado",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Fricassé",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Gelatina",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Galeto",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Guacamole",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Goiabada",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Guisado",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Hambúrguer",
+        categoria : "Comida",
+    },
+    {
+        palavra : "Iogurte",
+        categoria : "Comida",
+    },
+    {
+        palavra : "Jiló",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Jerimum",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Jaca",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Jabuticaba",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Jenipapo",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Kibe",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Kiwi",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Limão",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Linguiça",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Lasanha",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Lagosta",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Lámen",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Mamão",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Melancia",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Mandioca",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Milho",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Mousse",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Nuggets",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Nachos",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Nabo",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Noz",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Omelete",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Orégano",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Ovo",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Ostra",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Pasta de amendoim",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Palmito",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Pão",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Pimentão",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Peixe",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Queijo",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Quibe",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Quiabo",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Risoto",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Rabanete",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Ravioli",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Romã",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Salgadinho",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Sushi",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Sopa",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Salsicha",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Salada",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Tapioca",
+        categoria : "Comida",
+    },
+    {
+        palavra: "Torta",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Tacos",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Trufa",
+        categoria: "Comida",
+    },
+    {
+        palavra: "Tofu",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Uva",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Vatapá",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Vinho",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Vagem",
+        categoria: "Comida",
+    },
+    {
+        palavra : "Xerém",
+        categoria : "Comida"
+    },
+    {
+        palavra: "Yakisoba",
+        categoria: "Comida",
+    },
+]
+
+const palavras2 = [
+    {
+        palavra: "Anel",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Alicate",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Anzol",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Aparelho",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Almofada",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Bracelete",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Bacia",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Brinco",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Balde",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Bolo",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Carta",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Cadeira",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Computador",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Caneta",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Copo",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Dado",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Disco",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Despertador",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Dicionário",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Dentadura",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Escada",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Escova",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Espelho",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Esmalte",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Envelope",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Faca",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Fone de ouvido",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Frasco",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Fivela",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Frigideira",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Garfo",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Guarda chuva",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Guitarra",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Grampeador",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Gaiola",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Hidrante",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Haste",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Impressora",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Ioiô",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Ímã",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Janela",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Jornal",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Jaqueta",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Jarra",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Kimono",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Lâmpada",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Lápis",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Livro",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Lixeira",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Máscara",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Martelo",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Mochila",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Mouse",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Monitor",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Notebook",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Navio",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Óculos",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Odontoscópio",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Orelhão",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Ombreira",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Prato",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Panela",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Pincel",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Porta",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Pote",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Quadro",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Quadriciclo",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Relógio",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Roda",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Raquete",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Régua",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Roteador",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Sapato",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Serra",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Sapatilha",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Saxofone",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Smartwatch",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Telefone",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Televisão",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Tênis",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Toalha",
+        categoria: "Objeto"
+    },
+    {
+        palavra: "Tesoura",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Uniforme",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Ukulele",
+        categoria: "Objeto",
+    },
+    {
+        palavra: "Umidificador",
+        categoria: "Objeto",
+    },
+]
