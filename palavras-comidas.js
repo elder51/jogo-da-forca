9367409f845e3af37d56f8418f328d6eb58d227f
kyuta-081 <kevingabrielalves2002@gmail.com>
2024-07-24 17:09:15 -0300

    adicionando dicas

--- palavras-comidas.js
+++ palavras-comidas.js
@@ -1,115 +1,199 @@
 const palavras1 = [
     {
-        palavra : "Amendoim",
-        categoria : "Comida"
+        palavra: "Amendoim",
+        categoria: "Comida",
+        dica1: "Possui Casca",
+        dica2: "Fornece outras comidas",
+        dica3: "Paçoca"
     },
     {
         palavra: "Arroz",
         categoria: "Comida",
+        dica1: "Branco",
+        dica2: "Grão",
+        dica3: "Alomoço"
     },
     {
         palavra: "Amora",
         categoria: "Comida",
+        dica1: "Cor Preta",
+        dica2: "Antioxidante",
+        dica3: "Cacho",
     },
     {
         palavra: "Açaí",
         categoria: "Comida",
+        dica1: "Roxo",
+        dica2: "Doce Típico",
+        dica3: "Saudável"
     },
     {
         palavra: "Abacaxi",
         categoria: "Comida",
+        dica1: "Amarelo",
+        dica2: "Coroa",
+        dica3: "Possui Casca"
     },
     {
-        palavra : "Bolo",
-        categoria : "Comida"
+        palavra: "Bolo",
+        categoria: "Comida",
+        dica1: "Aniversário",
+        dica2: "Decorativo",
+        dica3: "Trigo, leite..."
     },
     {
         palavra: "Banana",
         categoria: "Comida",
+        dica1: "Potácil",
+        dica2: "Amarela",
+        dica3: "Cacho"
     },
     {
         palavra: "Brigadeiro",
         categoria: "Comida",
+        dica1: "Docinho de...",
+        dica2: "Chocolate",
+        dica3: "Confete"
     },
     {
         palavra: "Bolacha",
         categoria: "Comida",
+        dica1: "Salgada e Doce",
+        dica2: "Café ou lanche",
+        dica3: "Biscoito ou..."
     },
     {
         palavra: "Beringela",
         categoria: "Comida",
+        dica1: "Roxa",
+        dica2: "Prima do Pimentão",
+        dica3: "Formato suspeito"
     },
     {
         palavra : "Camarão",
-        categoria : "Comida"
+        categoria : "Comida",
+        dica1: "Laranja",
+        dica2: "Do mar",
+        dica3: "Crustáceo"
     },
     {
         palavra: "Coxinha",
         categoria: "Comida",
+        dica1: "Salgado",
+        dica2: "Grande ou pequena",
+        dica3: "100% brasileira"
     },
     {
         palavra: "Charque",
         categoria: "Comida",
+        dica1: "Carne",
+        dica2: "Salgada",
+        dica3: "Dessecada"
     },
     {
         palavra: "Cuscuz",
         categoria: "Comida",
+        dica1: "Derivado do milho",
+        dica2: "Nordestina",
+        dica3: "Flocão"
     },
     {
         palavra: "Cachorro quente",
         categoria: "Comida",
+        dica1: "Salsicha",
+        dica2: "Molho",
+        dica3: "Vai querer completo?"
     },
     {
         palavra: "Chocolate",
         categoria: "Comida",
+        dica1: "Doce",
+        dica2: "Branco e preto",
+        dica3: "bombom, barra..."
     },
     {
         palavra : "Damasco",
-        categoria : "Comida"
+        categoria : "Comida",
+        dica1: "Fruta",
+        dica2: "Laranja",
+        dica3: "Um caroço"
     },
     {
         palavra: "Dobradinha",
         categoria: "Comida",
+        dica1: "Feijão branco",
+        dica2: "Apimentada",
+        dica3: "Festa Junína"
     },
     {
         palavra: "Doce de leite",
         categoria: "Comida",
+        dica1: "Sobremesa",
+        dica2: "Pastoso ou sólido",
+        dica3: "Lata de vidro"
     },
     {
         palavra: "Donuts",
         categoria: "Comida",
+        dica1: "Redondo",
+        dica2: "Sobremesa",
+        dica3: "Americana"
     },
     {
         palavra : "Empada",
-        categoria : "Comida"
+        categoria : "Comida",
+        dica1: "Doce ou Salgada",
+        dica2: "Torta pequena",
+        dica3: "Crocante"
     },
     {
         palavra: "Enroladinho",
         categoria: "Comida",
+        dica1: "Salgado",
+        dica2: "Feiras",
+        dica3: "Salsicha"
     },
     {
         palavra: "Empadão",
         categoria: "Comida",
+        dica1: "Recheios variados",
+        dica2: "Frango",
+        dica3: "Empada grande"
     },
     {
         palavra: "Espaguete",
         categoria: "Comida",
+        dica1: "Massa",
+        dica2: "Almôndegas",
+        dica3: "Garfo"
     },
     {
         palavra: "Ervilha",
         categoria: "Comida",
+        dica1: "Verde",
+        dica2: "A princesa e a...",
+        dica3: "Milho e..."
     },
     {
         palavra : "Framboesa",
         categoria : "Comida",
+        dica1: "Vermelha",
+        dica2: "Cacho",
+        dica3: "Xarope, geleia..."
     },
     {
         palavra: "Feijão",
         categoria: "Comida",
+        dica1: "Almoço",
+        dica2: "Ferro e cálcio",
+        dica3: "Preto, branco, verde..."
     },
     {
         palavra: "Frango",
         categoria: "Comida",
+        dica1: "Carne",
+        dica2: "Recheio",
+        dica3: "Assado, cozido"
     },
     {
         palavra: "Figado",
@@ -359,6 +443,9 @@ const palavras1 = [
         palavra: "Yakisoba",
         categoria: "Comida",
     },
+]
+
+const palavras2 = [
     {
         palavra: "Anel",
         categoria: "Objeto",
@@ -699,176 +786,4 @@ const palavras1 = [
         palavra: "Umidificador",
         categoria: "Objeto",
     },
-    {
-        nome: "Banana",
-        categoria: "fruta",
-        dica1:"amarela",
-        dica2:"azul",
-        dica3:"pode por açucar"
-    },
-    {
-        nome: "Alface",
-        categoria: "verdura",
-        dica1: "verde",
-        dica2:"salada",
-        dica3:"forte"
-
-    },
-    {
-        nome: "Jumento",
-        categoria: "animal",
-        dica1:"orelhudo",
-        dica2:"cinza",
-        dica3:"shrek"
-
-
-    },
-    {
-        nome: "Inhame",
-        categoria: "comida",
-        dica1:"raiz",
-        dica2:"branco",
-        dica3:"nordestina"
-
-    },
-    {
-        nome: "Gargantilha",
-        categoria: "acessório" ,
-        dica1:"pescoço",
-        dica2:"sexy",
-        dica3:"colado"
-
-    },
-    {
-        nome: "Barba Ruiva",
-        categoria: "folclore",
-        dica1:"rio",
-        dica2:"laranja",
-        dica3:"homem"
-
-    },
-    {
-        nome: "Tacape",
-        categoria: "arma",
-        dica1:"idade da pedra",
-        dica2:"pedra e madeira",
-        dica3:"corda pedra de marmore"
-
-    },
-    {
-        nome: "Profissão",
-        categoria: "economista",
-        dica1:"tatuador",
-        dica2:"medico",
-        dica3:"desenvolverdor de sistemas"
-
-    },
-    {
-        nome: "Empecilho",
-        categoria: "bicho de sete cabeças",
-        dica1:"dificuldade",
-        dica2:"problema",
-        dica3:"dificil"
-
-    },
-    {
-        nome: "Psicanálise",
-        categoria: "estudo Teórico",
-        dica1:"piscicologico",
-        dica2:"mental",
-        dica3:"comportamental"
-
-    },
-    {
-        nome: "Cãibra",
-        categoria: "dor",
-        dica1:"muscular",
-        dica2:"torsão",
-        dica3:"choque"
-
-    },
-    {
-        nome: "Lazulita",
-        categoria: "Mineral",
-        dica1:"azul",
-        dica2:"tem no minecraft",
-        dica3:"faz encatamento"
-
-    },
-    {
-        nome: "Echarpe",
-        categoria: "objeto",
-        dica1:"rosangela usa",
-        dica2:"pano",
-        dica3:"acessorio"
-
-    },
-    {
-        nome: "Vestíbulo",
-        categoria: "pátio de acesso à entrada de uma construção",
-        dica1:"entrada",
-        dica2:"começo",
-        dica3:"inicio"
-
-    },
-    {
-        nome: "Narguilé",
-        categoria: "tipo de cachimbo",
-        dica1:"usado com escencia",
-        dica2:"fumaça",
-        dica3:"cano"
-
-    }
-    {
-        nome: "Cicatriz",
-        categoria: "marca permanente",
-    },
-    {
-        nome: "Microfone",
-        categoria: "é usado em stand-ups, para ampliação",
-    },
-    {
-        origin: "Zigue-Zague",
-        nome: "Zigue-Zague",
-        categoria: "linha ou série de linhas quebradas fazem...",
-    },
-    {
-        nome: "Magnético",
-        categoria: "que puxa, agarra, fascina e atrai",
-    },
-    {
-        nome: "Balaústre",
-        categoria: "sustenta um peitoril ou corrimão",
-    },
-    {
-        nome: "Oboé",
-        categoria: "instrumento musical",
-    },
-    {
-        nome: "Go",
-        categoria: "jogo estrátegico para tabuleiro",
-    },
-    {
-        nome: "Taiko",
-        categoria: "tambor de origem japonesa",
-    },
-    {
-        origin: "Helena Ignez",
-        nome: "Helena Ignez",
-        categoria: "Atriz Brasileira",
-    },
-    {
-        origin: "Júlio Bressane",
-        nome: "Júlio Bressane",
-        categoria: "Diretor de Cinema Brasileiro",
-    },
-    {
-        origin: "Rogério Sganzerla",
-        nome: "Rogério Sganzerla",
-        categoria: "Diretor de Cinema Brasileiro",
-    },
-    {
-        nome: "Epítome",
-        categoria: "Resume noções básicas de um assunto, de uma arte etc",
-    }
 ]
