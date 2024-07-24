da2dcc107e7b5fb0c58cb2423a2f9ecb420a3b2a
Macatucano <duduuhuah@gmail.com>
2024-07-24 15:59:11 -0300

    Update palavras-comidas.js

--- palavras-comidas.js
+++ palavras-comidas.js
@@ -359,9 +359,6 @@ const palavras1 = [
         palavra: "Yakisoba",
         categoria: "Comida",
     },
-]
-
-const palavras2 = [
     {
         palavra: "Anel",
         categoria: "Objeto",
@@ -702,4 +699,116 @@ const palavras2 = [
         palavra: "Umidificador",
         categoria: "Objeto",
     },
+    {
+        nome: "Alface",
+        categoria: "verdura",
+    },
+    {
+        nome: "Jumento",
+        categoria: "animal",
+    },
+    {
+        nome: "Inhame",
+        categoria: "comida",
+    },
+    {
+        nome: "Gargantilha",
+        categoria: "acessório",
+    },
+    {
+        nome: "Barba Ruiva",
+        categoria: "folclore"
+    },
+    {
+        nome: "Tacape",
+        categoria: "arma",
+    },
+    {
+        nome: "Profissão",
+        categoria: "economista",
+    },
+    {
+        nome: "Empecilho",
+        categoria: "bicho de sete cabeças",
+    },
+    {
+        nome: "Psicanálise",
+        categoria: "estudo Teórico",
+    },
+    {
+        nome: "Cãibra",
+        categoria: "dor",
+    },
+    {
+        nome: "Lazulita",
+        categoria: "mineral",
+    },
+    {
+        nome: "Jazz",
+        categoria: "estilo musical",
+    },
+    {
+        nome: "Echarpe",
+        categoria: "objeto",
+    },
+    {
+        nome: "Vestíbulo",
+        categoria: "pátio de acesso à entrada de uma construção",
+    },
+    {
+        nome: "Narguilé",
+        categoria: "tipo de cachimbo",
+    },
+    {
+        nome: "Cicatriz",
+        categoria: "marca permanente",
+    },
+    {
+        nome: "Microfone",
+        categoria: "é usado em stand-ups, para ampliação",
+    },
+    {
+        origin: "Zigue-Zague",
+        nome: "Zigue-Zague",
+        categoria: "linha ou série de linhas quebradas fazem...",
+    },
+    {
+        nome: "Magnético",
+        categoria: "que puxa, agarra, fascina e atrai",
+    },
+    {
+        nome: "Balaústre",
+        categoria: "sustenta um peitoril ou corrimão",
+    },
+    {
+        nome: "Oboé",
+        categoria: "instrumento musical",
+    },
+    {
+        nome: "Go",
+        categoria: "jogo estrátegico para tabuleiro",
+    },
+    {
+        nome: "Taiko",
+        categoria: "tambor de origem japonesa",
+    },
+    {
+        origin: "Helena Ignez",
+        nome: "Helena Ignez",
+        categoria: "Atriz Brasileira",
+    },
+    {
+        origin: "Júlio Bressane",
+        nome: "Júlio Bressane",
+        categoria: "Diretor de Cinema Brasileiro",
+    },
+    {
+        origin: "Rogério Sganzerla",
+        nome: "Rogério Sganzerla",
+        categoria: "Diretor de Cinema Brasileiro",
+    },
+    {
+        nome: "Epítome",
+        categoria: "Resume noções básicas de um assunto, de uma arte etc",
+    }
 ]
